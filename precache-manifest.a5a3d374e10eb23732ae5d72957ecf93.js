62c960c61b1eb5277e851aeaefa3a0ba5027236e
Vytautas <vytautasjaz@gamil.com>
2020-10-05 17:09:28 +0300

    Updates

---
+++ precache-manifest.a5a3d374e10eb23732ae5d72957ecf93.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "694412a5a14b6f8376208d4865df64fe",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f393708c89814250b2ab",
+    "url": "/static/css/main.80fdc21c.chunk.css"
+  },
+  {
+    "revision": "24f7e27a9ee08d528ddd",
+    "url": "/static/js/2.b91c821a.chunk.js"
+  },
+  {
+    "revision": "6cc9b2cb4db1447b734d925a70c8c404",
+    "url": "/static/js/2.b91c821a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f393708c89814250b2ab",
+    "url": "/static/js/main.d171d348.chunk.js"
+  },
+  {
+    "revision": "0d6909884b6b6b3c8033",
+    "url": "/static/js/runtime-main.07623122.js"
+  },
+  {
+    "revision": "b9d188e572906dec1e44eb658c488557",
+    "url": "/static/media/profile_large.b9d188e5.png"
+  }
+]);
